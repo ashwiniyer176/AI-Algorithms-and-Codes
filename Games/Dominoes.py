d661d6cd581be9dffcbc70eb37fc499698efcb32
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2021-05-08 07:53:42 +0530

    Add files via upload

--- Games/Dominoes.py
+++ Games/Dominoes.py
@@ -1,7 +1,99 @@
 import random
 
 
+class State:
+
+    def __init__(self, currentState, moveRequired=(-1, -1)):
+        self.currentState = currentState
+        self.nextStates = []
+        self.moveRequired = moveRequired
+
+    def printState(self):
+        print(self.currentState)
+        print("Move Required to reach:", self.moveRequired)
+
+
+class GameTree:
+    visitedStates = []
+
+    def __init__(self, currentState, currentDominoes, otherDominoes):
+        self.rootState = State(currentState)
+        self.currentDominoes = currentDominoes
+        self.otherDominoes = otherDominoes
+
+    def makeTree(self):
+        queue = []
+        self.makeNextStates(
+            self.rootState, self.currentDominoes, self.otherDominoes, queue)
+
+        if(len(queue) > 2):
+            queue.pop(-1)
+        queue.pop(0)
+        return queue
+
+    def findAllPlayableDominoes(self, currentState, dominoes):
+        left = currentState[0][0]
+        right = currentState[-1][1]
+        playableDominoes = []
+        for domino in dominoes:
+            if(left != right):
+                if(domino[1] == left or domino[0] == right):
+                    playableDominoes.append(domino)
+                elif(domino[0] == left or domino[1] == right):
+                    reverseDomino = (domino[1], domino[0])
+                    playableDominoes.append(reverseDomino)
+            else:
+                if(domino[0] == left or domino[1] == left):
+                    playableDominoes.append(domino)
+        return playableDominoes
+
+    def removeDominoFromSet(self, domino, dominoSet):
+        reverseDomino = (domino[1], domino[0])
+        if(domino in dominoSet):
+            dominoSet.remove(domino)
+        elif(reverseDomino in dominoSet):
+            dominoSet.remove(reverseDomino)
+
+    def makeNextStates(self, currentState, currentDominoes, otherDominoes, queue):
+        if(len(otherDominoes) == 0):
+            queue.extend([currentState.moveRequired, 1])
+            return
+        elif(len(currentDominoes) == 0):
+            queue.extend([currentState.moveRequired, 0])
+            return
+        else:
+            playableDominoes = self.findAllPlayableDominoes(
+                currentState.currentState, currentDominoes)
+            for domino in playableDominoes:
+                self.addDominoesToState(domino, currentState, currentDominoes)
+            queue.append(currentState.moveRequired)
+            for state in currentState.nextStates:
+                self.makeNextStates(state, currentDominoes,
+                                    otherDominoes, queue)
+                if(queue[-1] == 1 or queue[-1] == 0):
+                    break
+
+    def addDominoesToState(self, domino, state, dominoSet):
+        left = state.currentState[0][0]
+        right = state.currentState[-1][1]
+        reverseDomino = (domino[1], domino[0])
+        newState = state.currentState[:]
+        if(left == domino[1]):
+            newState.insert(0, domino)
+        elif(left == reverseDomino[1]):
+            newState.insert(0, reverseDomino)
+        elif(right == domino[0]):
+            newState.append(domino)
+        else:
+            newState.append(reverseDomino)
+        self.removeDominoFromSet(domino, dominoSet)
+        newState = State(newState, domino)
+        state.nextStates.append(newState)
+
+
 class Player:
+    moveList = []
+
     def __init__(self, playerName):
         self.playername = playerName
         self.__dominoes = []
@@ -55,15 +147,38 @@ class Player:
         bestDomino = self.returnBestPlayableDomino(playableDominoes)
         return bestDomino
 
-    def makeMove(self, currentState):
+    def chooseAMove(self, moveAI, moveAlgorithm):
+        if(moveAI == moveAlgorithm):
+            return moveAI
+        else:
+            if(moveAI[0] < 0):
+                return moveAlgorithm
+            elif(moveAlgorithm[0] < 0):
+                return moveAI
+            else:
+                return moveAI
+
+    def makeMove(self, currentState, otherDominoes):
         print(f"\n\n{self.playername} moves now")
-        lowerEnd = currentState[0][0]
-        upperEnd = currentState[-1][1]
-        bestDomino = self.findBestDomino(lowerEnd, upperEnd)
-        return bestDomino
+        nextMove = (-1, 0)
+        # if(len(self.moveList) == 0):
+        yourDominoes = otherDominoes[:]
+        myDominoes = self.returnDominoesLeft()[:]
+        g = GameTree(currentState, myDominoes, yourDominoes)
+        self.moveList = g.makeTree()
+        if(len(self.moveList) > 0):
+            nextMove = self.moveList.pop(0)
+        print("AI Suggests:", nextMove)
+
+        bestDomino = self.findBestDomino(
+            currentState[0][0], currentState[-1][1])
+        return nextMove
+
+    def returnNoOfDominoesLeft(self):
+        return len(self.__dominoes)
 
     def returnDominoesLeft(self):
-        return len(self.__dominoes)
+        return self.__dominoes
 
 
 class Dominoes:
@@ -125,7 +240,8 @@ class Dominoes:
 
     def decidePlayer(self):
         if(self.maxPlaying):
-            newMove = self.maxPlayer.makeMove(self.currentState)
+            newMove = self.maxPlayer.makeMove(
+                self.currentState, self.minPlayer.returnDominoesLeft())
             if(newMove[0] >= 0):
                 print("Max player move:", newMove)
                 self.maxPlayer.printDominoes()
@@ -137,7 +253,8 @@ class Dominoes:
                 self.maxMissed = True
                 self.maxPlaying = False
         else:
-            newMove = self.minPlayer.makeMove(self.currentState)
+            newMove = self.minPlayer.makeMove(
+                self.currentState, self.maxPlayer.returnDominoesLeft())
             if(newMove[0] >= 0):
                 print("Min player move:", newMove)
                 self.minPlayer.printDominoes()
@@ -155,10 +272,10 @@ class Dominoes:
             print(self.currentState)
             if(self.minMissed and self.maxMissed):
                 exit(1)
-            if(self.minPlayer.returnDominoesLeft() == 0):
+            if(self.minPlayer.returnNoOfDominoesLeft() == 0):
                 print("Game Over. Min Wins!")
                 exit(1)
-            elif(self.maxPlayer.returnDominoesLeft() == 0):
+            elif(self.maxPlayer.returnNoOfDominoesLeft() == 0):
                 print("Game Over. Max Wins!")
                 exit(1)
 
